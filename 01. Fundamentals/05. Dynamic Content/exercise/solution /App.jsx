77abfb7ec771853449585db0410561f2eb74934d
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-10-17 16:20:33 +0300

    Create App.jsx

---
+++ 01. Fundamentals/05. Dynamic Content/exercise/solution /App.jsx
@@ -0,0 +1,13 @@
+import Greet from "./components/Greet";
+import ProductInfo from "./components/ProductInfo";
+
+const App = () => {
+  return (
+    <div>
+      <Greet />
+      <ProductInfo />
+    </div>
+  );
+};
+
+export default App;
