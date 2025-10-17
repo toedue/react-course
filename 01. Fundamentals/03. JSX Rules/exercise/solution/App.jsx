7e9648e90195fdb7e76e784c499699efd4f50e71
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-10-17 15:14:32 +0300

    Create App.jsx

---
+++ 01. Fundamentals/03. JSX Rules/exercise/solution/App.jsx
@@ -0,0 +1,12 @@
+import JSXRules from "./components/JSXRules";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+const App = () => {
+  return (
+    <div>
+      <JSXRules />
+    </div>
+  );
+};
+
+export default App;
