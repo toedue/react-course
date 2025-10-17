da5470f9600197f55fff3b6784deda7fba1f9819
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-10-17 15:14:07 +0300

    Create JSXRules.jsx

---
+++ 01. Fundamentals/03. JSX Rules/exercise/solution/JSXRules.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p>JSX must return a single parent element</p>
+      <p>JSX elements must be property closed</p>
+      <p>
+        JSX attributes are written using camelCase (e.g., className instead of
+        class).
+      </p>
+    </div>
+  );
+};
+
+export default JSXRules;
